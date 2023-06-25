3c38af6c272e5086eea6fd406559e393491d2855
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-25 15:58:19 +0300

    add interface and validation for function arguments

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 import axios from "axios";
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+
 axios.get(url).then(respose =>{
-    console.log(respose.data);
-})
+    const todo = respose.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+        the doto with id: ${id}
+        has a title of : ${title}
+        is it finished: ${completed}
+`);
+};
